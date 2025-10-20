4bfc3346d619be2de2175e75730e10efa1a61534
rahmanef63 <rahmanef63@gmail.com>
2025-10-20 04:05:49 +0300

    restructure shared

--- frontend/shared/layout/MasterDetailLayout.tsx
+++ frontend/shared/layout/MasterDetailLayout.tsx
@@ -62,3 +62,5 @@ export function MasterDetailLayout({
     </div>
   )
 }
+
+export default MasterDetailLayout
--- frontend/shared/layout/index.ts
+++ frontend/shared/layout/index.ts
@@ -6,12 +6,12 @@ export * as Menus from "./menus";
 
 // View layer (table/card/switchers)
 export * from "./view";
-export *  TableView from "./view/TableView";
-export *  CardView from "./view/CardView";
-export *  DetailListView from "./view/DetailListView";
-export *  ViewSwitcher from "./view/ViewSwitcher";
-export *  ViewToolbar from "./view/ViewToolbar";
-export *  RowActions from "./view/RowActions";
+export { TableView } from "./view/TableView";
+export { CardView } from "./view/CardView";
+export { DetailListView } from "./view/DetailListView";
+export { ViewSwitcher } from "./view/ViewSwitcher";
+export { ViewToolbar } from "./view/ViewToolbar";
+export { RowActions } from "./view/RowActions";
 
 // Notifications
 export * from "./notifications";
